630ce9dae8723f324badfc218618cf8133ebb4b3
Gabriel Godoy <gabyreload@gmail.com>
2022-12-11 17:16:22 -0300

    Adding Badge to CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,13 @@
+import { Badge } from "react-bootstrap";
 import { BsCart3 } from "react-icons/bs";
 
 const CartWidget = () => {
-  return <BsCart3 size={25} />;
+  return (
+    <>
+      <BsCart3 size={25} />
+      <Badge className="ms-2">5</Badge>
+    </>
+  );
 };
 
 export default CartWidget;
