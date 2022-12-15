62e5b18413b64b428f7d50fadd65114874b5dfca
Gabriel Godoy <gabyreload@gmail.com>
2022-12-15 00:03:12 -0300

    Conditional rendering
    Adding buttons Vaciar carrito,  Seguir Comprando and Terminar Comprar

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useContext } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 
 function Cart(props) {
@@ -10,52 +11,64 @@ function Cart(props) {
 
   return (
     <Container className="mt-2">
-      <h1>MI CARRITO</h1>
+      <h2>MI CARRITO</h2>
       {cartList.length !== 0 && (
         <Row>
-          <Col className="d-flex justify-content-between">
-            <Button className="mb-4" variant="outline-danger" onClick={clear}>
-              Vaciar carrito
-            </Button>
-            <Button className="mb-4" variant="outline-success">
-              Comprar
-            </Button>
+          <Col className="mb-4">
+            <ButtonGroup>
+              <Button variant="outline-danger" onClick={clear}>
+                Vaciar carrito
+              </Button>
+              <Button variant="outline-info" as={Link} to="/">
+                Seguir Comprando
+              </Button>
+            </ButtonGroup>
           </Col>
         </Row>
       )}
       {cartList.length ? (
-        <Table striped bordered hover responsive>
-          <thead>
-            <tr>
-              <th>QUITAR</th>
-              <th>CANTIDAD</th>
-              <th>DESCRIPCION</th>
-              <th>PRECIO</th>
-              <th>SUBTOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cartList.map((item) => (
-              <tr key={item.id}>
-                <td>
-                  <Button onClick={() => removeItem(item.id)}>X</Button>
-                </td>
-                <td>{item.quantity}</td>
-                <td>{item.title}</td>
-                <td>$ {item.price}</td>
-                <td>$ {calcSubtotal(item.id)}</td>
+        <>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>QUITAR</th>
+                <th>CANTIDAD</th>
+                <th>DESCRIPCION</th>
+                <th>PRECIO</th>
+                <th>SUBTOTAL</th>
               </tr>
-            ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan={4}>TOTAL</td>
-              <td>$ {calcTotal()}</td>
-            </tr>
-          </tfoot>
-        </Table>
+            </thead>
+            <tbody>
+              {cartList.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Button onClick={() => removeItem(item.id)}>X</Button>
+                  </td>
+                  <td>{item.quantity}</td>
+                  <td>{item.title}</td>
+                  <td>$ {item.price}</td>
+                  <td>$ {calcSubtotal(item.id)}</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={4}>TOTAL</td>
+                <td>$ {calcTotal()}</td>
+              </tr>
+            </tfoot>
+          </Table>
+          <Col className="d-flex justify-content-end">
+            <Button variant="outline-success">Terminar Comprar</Button>
+          </Col>
+        </>
       ) : (
-        <p>Carrito vacio</p>
+        <>
+          <span>Carrito vacio</span>
+          <Button variant="outline-info ms-3" as={Link} to="/">
+            Seguir Comprando
+          </Button>
+        </>
       )}
     </Container>
   );
