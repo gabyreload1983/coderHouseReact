57f64f590dae49f444a3bced5c9d3e8712872985
Gabriel Godoy <gabyreload@gmail.com>
2022-12-20 23:53:22 -0300

    createOrder function

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
-import { Link } from "react-router-dom";
+import {
+  collection,
+  increment,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../utils/firebaseConfig";
+import { BarLoader } from "react-spinners";
+import { ToastContainer, toast } from "react-toastify";
 import { CartContext } from "../context/CartContext";
 
 function Cart(props) {
   const { cartList, removeItem, clear, calcSubtotal, calcTotal } =
     useContext(CartContext);
+  const [loading, setLoading] = useState(false);
+
+  const createOrderInFirestore = async (order) => {
+    const newOrderRef = doc(collection(db, "orders"));
+    await setDoc(newOrderRef, order);
+    return newOrderRef;
+  };
+
+  const createOrder = async () => {
+    try {
+      setLoading(true);
+      const order = {
+        buyer: {
+          name: "Leo Messi",
+          email: "leomessi@gmail.com",
+          phone: "1234567",
+        },
+        date: serverTimestamp(),
+        products: cartList.map((p) => ({
+          id: p.id,
+          title: p.title,
+          price: p.price,
+          quantity: p.quantity,
+        })),
+        total: calcTotal(),
+      };
+
+      const res = await createOrderInFirestore(order);
+      if (res) {
+        cartList.forEach(async (p) => {
+          const productRef = doc(db, "products", p.id);
+          await updateDoc(productRef, {
+            stock: increment(-p.quantity),
+          });
+        });
+        setLoading(false);
+        clear();
+        toast.success(
+          `Gracias por tu compra.
+        El numero de tu orden de compra es: ${res.id}`,
+          {
+            position: "top-center",
+            autoClose: false,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+          }
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <Container className="mt-2">
+    <Container className="mt-5">
+      {loading && <BarLoader width="100%" color="#36d7b7" className="mb-3" />}
       <h2>MI CARRITO</h2>
       {cartList.length !== 0 && (
         <Row>
@@ -59,7 +126,13 @@ function Cart(props) {
             </tfoot>
           </Table>
           <Col className="d-flex justify-content-end">
-            <Button variant="outline-success">Terminar Comprar</Button>
+            <Button
+              variant="outline-success"
+              onClick={createOrder}
+              disabled={loading ? true : false}
+            >
+              Terminar Comprar
+            </Button>
           </Col>
         </>
       ) : (
@@ -70,6 +143,16 @@ function Cart(props) {
           </Button>
         </>
       )}
+      <ToastContainer
+        position="top-center"
+        autoClose={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="dark"
+      />
     </Container>
   );
 }
